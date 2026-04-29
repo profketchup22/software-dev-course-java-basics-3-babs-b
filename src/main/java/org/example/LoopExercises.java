9403b267bbe2cb712ccdf53a724df5c5dfd6641f
profketchup22 <profketchup22@gmail.com>
2026-04-28 20:58:54 -0500

    Complete conditional and loop exercises

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -4,13 +4,25 @@ public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // (use a for loop)
-        return 0;
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total = total + i;
+        }
+        return total;
     }
 
     public int sumUntilEven(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
-        return 0;
+            int sum = 0;
+            int counter = 1;
+
+            while (sum % 2 != 0 || sum == 0) {
+                sum += counter;
+                counter++;
+            }
+
+            return sum;
+        }
     }
-}
